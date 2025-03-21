6414b4ee0349d07203003a02c2c1cac8dc7be3c0
Hakam AboAlia <hakamabualia538@gmail.com>
2025-03-21 22:20:32 +0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,16 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
+		LinkedList<Integer> list = new LinkedList<>();
+		list.insert(77);
+		list.insert(65);
+		list.insert(77);
+		list.insert(77);
+		list.insert(99);
+		list.delete(99);
+		System.out.println(list);
+		list.getLength();
+		list.removeDuplicates();
+		System.out.println(list);
 	}
 }
